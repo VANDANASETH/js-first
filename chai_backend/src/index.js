38728c02ddf2a5c8408b1da087171d2e0b19c027
VANDANASETH <161039697+VANDANASETH@users.noreply.github.com>
2024-09-12 09:16:42 +0000

    utils

--- chai_backend/src/index.js
+++ chai_backend/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 
 dotenv.config({
@@ -7,6 +8,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!!", err)
+})
 
 /*
 import express from "express"
