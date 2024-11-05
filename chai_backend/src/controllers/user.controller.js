a3b2ef85aadc26bb06324134e9391f4044c2d0f9
VANDANASETH <161039697+VANDANASETH@users.noreply.github.com>
2024-11-05 07:54:30 +0000

    authmiddleware

--- chai_backend/src/controllers/user.controller.js
+++ chai_backend/src/controllers/user.controller.js
@@ -128,7 +128,28 @@ const loginUser = asyncHandler(async (req, res) =>{
 })
 
 const logoutUser = asyncHandler(async(req, res)=>{
-    
+    await User.findByIdAndDelete(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out"))
 })
 export {
     registerUser,
